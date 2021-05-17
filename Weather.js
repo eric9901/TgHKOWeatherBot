de1c803fa7938f2f7077337f7d01653b9ae28c4c
eric9901 <erickwok9902@gmail.com>
2021-05-17 21:17:52 +0800

    Update Weather.js
    change some content of nine day prediction

--- Weather.js
+++ Weather.js
@@ -22,20 +22,34 @@ bot.onText(/\/echo(.+)/,function(msg,match){
     var echo = match[1];
     bot.sendMessage(chatId,echo);
 });
-
-bot.onText(/\/weather(.+)/,function(msg,match){
+// now weather report
+bot.onText(/\/weare(.+)/,function(msg,match){
+    var chatId = msg.chat.id;
+    var wCode = match[1];
+    request(`https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc`,function(error,response,body){
+            if(!error && response.statusCode ==200){
+            bot.sendMessage(chatId,'_偽毒同你睇緊而家天氣_',{parse_mode:'Markdown'})
+            .then(function(msg){
+                var res=JSON.parse(body);
+                bot.sendMessage(chatId,'偽毒話你知而家天氣係'+res.updateTime);
+            })
+            }
+        });
+    bot.sendMessage(chatId,echo);
+});
+//day weather prediction 
+bot.onText(/\/weaPre(.+)/,function(msg,match){
     var chatId=msg.chat.id;
     var weather =match[1];
     let wCode='c';
-
     if (weather===' a')   {
         wCode='flw';
         request(`https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=${wCode}&lang=tc`,function(error,response,body){
             if(!error && response.statusCode ==200){
-            bot.sendMessage(chatId,'_睇緊今日天氣_',{parse_mode:'Markdown'})
+            bot.sendMessage(chatId,'_偽毒同你睇緊今日天氣預報_',{parse_mode:'Markdown'})
             .then(function(msg){
                 var res=JSON.parse(body);
-                bot.sendMessage(chatId,'今日天氣係'+res.generalSituation);
+                bot.sendMessage(chatId,'偽毒話你知今日天氣預報係'+res.generalSituation);
             })
             }
         });
@@ -44,10 +58,13 @@ bot.onText(/\/weather(.+)/,function(msg,match){
         wCode ='fnd';
         request(`https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=${wCode}&lang=tc`,function(error,response,body){
             if(!error && response.statusCode ==200){
-            bot.sendMessage(chatId,'_睇緊九日天氣_',{parse_mode:'Markdown'})
+            bot.sendMessage(chatId,'_偽毒同你睇緊九日天氣預報_',{parse_mode:'Markdown'})
             .then(function(msg){
                 var res=JSON.parse(body);
-                bot.sendMessage(chatId,'黎緊九日天氣係'+res.generalSituation);
+            
+                //var tem=JSON.parse(res);
+               
+                bot.sendMessage(chatId,'偽毒話你知黎緊九日天氣預報係'+res["weatherForecast"][1]["forecastWeather"]);
             })
             }
         });
