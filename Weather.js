62d87add9ac4900a689eabbad16ac9687fd8a9cc
eric9901 <erickwok9902@gmail.com>
2021-05-27 15:11:22 +0800

    Update Weather.js
    added command /Help to show the command list of the bot

--- Weather.js
+++ Weather.js
@@ -2,44 +2,31 @@ var TelegramBot = require('node-telegram-bot-api');
 var token='1250642246:AAEcHZo4fZinIowfm_-q-ECm_jbUvdGF59A';
 var bot = new TelegramBot(token, {polling:true});
 var request = require('request');
-bot.onText(/\/movie(.+)/,function(msg,match){
-    var movie =match[1];
-    var chatId=msg.chat.id;
-    request(`http://www.omdbapi.com/?apikey=67b859ad&t=${movie}`,function(error,response,body){
-     if(!error && response.statusCode ==200){
-         bot.sendMessage(chatId, '_Looking for _' + movie+ '...',{parse_mode:'Markdown'})
-         .then(function(msg){
-            var res=JSON.parse(body);
-            bot.sendMessage(chatId, );
-            bot.sendPhoto(chatId, res.Poster,{caption: 'Result:\nTitle ' + res.Title +'\nYear: '+res.Year+'\nRaterd: '+res.Rated+'\nReleased: '+res.Released })
-         })
-     }
-    });
+
+//show the command and explaintion
+bot.onText(/\/Help/,(msg)=>{
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId,"command list:\n /weare: current each distanct temperature\n /WeaPre fd: general today weather prediction\n /Weapre: detail today weather prediction\n /wea9DPre x:maximun 9 day weather prediction, x is the number of day beyond today that youwant to predict.");
+
 });
 
-//message part
-/*
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-  
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
-});*/
-// now weather report
+// current weather report
 bot.onText(/\/weare/,(msg)=>{
     var chatId = msg.chat.id;
 
     request(`https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc`,function(error,response,body){
             if(!error && response.statusCode ==200){
-            bot.sendMessage(chatId,'_偽毒同你睇緊而家天氣_',{parse_mode:'Markdown'})
+            bot.sendMessage(chatId,'_偽毒話你知而家十八個天交台觀察點氣溫係_',{parse_mode:'Markdown'})
             .then(function(msg){
                 var res=JSON.parse(body);
-                bot.sendMessage(chatId,'偽毒話你知而家天氣係'+res["temperature"]["data"][2]["place"]);
+                res=res["temperature"]["data"];
+                for (i = 0; i < 18; i++) {
+                bot.sendMessage(chatId,res[i]["place"]+':'+res[i]["value"]+'c');}
             })
            }
         });
 });
-//day weather prediction 
+//general informatyion of today weather prediction  
 bot.onText(/\/weaPre(.+)/,function(msg,match){
     var chatId=msg.chat.id;
     var Command =match[1];
@@ -58,7 +45,7 @@ bot.onText(/\/weaPre(.+)/,function(msg,match){
             }
         });
 });
-
+//detail informatyion of today weather prediction  
 bot.onText(/\/weaPre/,function(msg){
     var chatId=msg.chat.id;
     var W="/weaPre";
@@ -81,7 +68,7 @@ bot.onText(/\/weaPre/,function(msg){
     }
 });
 
-
+// nine day weather perdiction
 bot.onText(/\/wea9DPre(.+)/,function(msg,match){
     var chatId=msg.chat.id;
     var nextDay =parseInt(match[1])-1;
